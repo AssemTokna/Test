11bc7391f84459dcdbb44c00b07c59c5d0711acb
AssemTokna <assemomar9900@gmail.com>
2024-04-25 20:38:13 +0200

    test

---
+++ reactweb/src/App.jsx
@@ -0,0 +1,15 @@
+
+import LoginRegister from './component/LoginRegister/LoginRegister'
+
+function App() {
+
+
+  return (
+    
+      <div>
+     <LoginRegister/>
+   </div>
+  );
+}
+
+export default App
